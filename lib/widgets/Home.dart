9c9d75761f587756652bc8eecd9819459e17402a
paprikaLang <3060866223@qq.com>
2018-12-18 14:46:22 +0800

    listtile drawerheader

--- lib/widgets/Home.dart
+++ lib/widgets/Home.dart
@@ -42,6 +42,32 @@ class Home extends StatelessWidget {
             Icon(Icons.directions_bike, size: 100.0, color: Colors.black26),
           ],
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Tiyo'.toUpperCase()),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                ),
+              ),
+              ListTile(
+                title: Text('Message', textAlign: TextAlign.right),
+                trailing: Icon(Icons.message, color: Colors.black12, size: 22.0),
+              ),
+              ListTile(
+                title: Text('Settings', textAlign: TextAlign.right),
+                trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0),
+              ),
+              ListTile(
+                title: Text('Favorite', textAlign: TextAlign.right),
+                trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0),
+              ),
+
+            ],
+          ),
+        ),
       ),
     );
   }
