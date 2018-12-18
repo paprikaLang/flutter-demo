3445c2a802d743548df087780c1beb02d634d373
paprikaLang <3060866223@qq.com>
2018-12-18 09:02:13 +0800

    tabbarview.children

--- lib/widgets/Home.dart
+++ lib/widgets/Home.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 import 'ListContainer.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('paprikaLang.github.io'),
-        elevation: 0.0, // 阴影
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation',
+            onPressed: () => debugPrint('press in'),
+          ),
+          title: Text('paprikaLang.github.io'),
+          elevation: 0.0, // 阴影
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'actions for search',
+              onPressed: () => debugPrint('search in'),
+            )
+          ],
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ListContainer(),
+            Icon(Icons.change_history, size: 100.0, color: Colors.black26),
+            Icon(Icons.directions_bike, size: 100.0, color: Colors.black26),
+          ],
+        ),
       ),
-      body: ListContainer(),
     );
   }
 }
