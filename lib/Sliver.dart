afea1c734f1ec2ee463692ed96955034e2acf0dd
paprikaLang <3060866223@qq.com>
2018-12-19 15:38:21 +0800

    sliverSafeArea sliverpadding slivergrid

--- lib/Sliver.dart
+++ lib/Sliver.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import './model/post.dart';
 
 class SliverWork extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverSafeArea(
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(8),
+              sliver: SliverGridComponent(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SliverGridComponent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+          childAspectRatio: 0.8),
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        return Container(
+          child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+        );
+      }, childCount: posts.length),
+    );
   }
 }
