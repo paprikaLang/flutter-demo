2415e22a89d680edaf6031fad1b6394733cdd2cc
paprikaLang <3060866223@qq.com>
2018-12-20 00:20:57 +0800

    textfield decoration/texteditingcontroller.addlistener

--- lib/widgets/LogIn/login_form.dart
+++ lib/widgets/LogIn/login_form.dart
@@ -9,7 +9,13 @@ class LoginForm extends StatelessWidget {
       ),
       body: Theme(
         data: Theme.of(context).copyWith(primaryColor: Colors.black),
-        child: ThemeDemo(),
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[TextdfieldDemo()],
+          ),
+        ),
       ),
     );
   }
@@ -20,6 +26,50 @@ class ThemeDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Theme.of(context).accentColor,
+    );
+  }
+}
+
+class TextdfieldDemo extends StatefulWidget {
+  @override
+  _TextdfieldDemoState createState() => _TextdfieldDemoState();
+}
+
+class _TextdfieldDemoState extends State<TextdfieldDemo> {
+  final _textfieldController = TextEditingController();
+  @override
+  void dispose() {
+    _textfieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _textfieldController.addListener(() {
+      debugPrint('input listener: ${_textfieldController.text}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _textfieldController,
+      // onChanged: (value) {
+      //   debugPrint('input: $value');
+      // },
+      onSubmitted: (value) {
+        // 提交时统计输出的内容
+        debugPrint('submit: $value');
+      },
+      decoration: InputDecoration(
+          icon: Icon(Icons.verified_user),
+          labelText: 'Username',
+          hintText: 'Enter the username',
+          filled: true,
+          fillColor: Color.fromRGBO(233, 233, 233, 0.3),
+          // border: OutlineInputBorder(),
+          border: InputBorder.none),
     );
   }
 }
