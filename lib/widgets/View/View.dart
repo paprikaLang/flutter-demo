0174f2584f1d662f2e612f590ee761a93836c209
paprikaLang <3060866223@qq.com>
2018-12-19 11:33:25 +0800

    pageview itembuilder

--- lib/widgets/View/View.dart
+++ lib/widgets/View/View.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
+import '../../model/post.dart';
 
 class View extends StatelessWidget {
+  Widget _itembuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          // expand 占满所有可用空间
+          child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+        ),
+        Positioned(
+          bottom: 20,
+          left: 20,
+          child: Column(
+            children: <Widget>[
+              Text(posts[index].author,
+                  style: TextStyle(color: Colors.white, fontSize: 22.0)),
+              Text(posts[index].title,
+                  style: TextStyle(color: Colors.white, fontSize: 22.0))
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _itembuilder,
+    );
+  }
+}
+
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
@@ -9,6 +43,8 @@ class View extends StatelessWidget {
       reverse: true,
       scrollDirection: Axis.vertical, //垂直翻滚页面
       onPageChanged: (currentPage) => debugPrint('Page: $currentPage'),
+      controller: PageController(
+          initialPage: 1, keepPage: true, viewportFraction: 0.85),
       children: <Widget>[
         Container(
           color: Colors.brown[200],
