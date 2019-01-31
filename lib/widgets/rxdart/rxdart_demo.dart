f8466dbe1ca6fa062981a9bc3dcd6241cb063c40
paprikaLang <3060866223@qq.com>
2019-01-31 15:33:47 +0800

    where debounce map

--- lib/widgets/rxdart/rxdart_demo.dart
+++ lib/widgets/rxdart/rxdart_demo.dart
@@ -22,9 +22,17 @@ class RxDartDemoHome extends StatefulWidget {
 }
 
 class _RxDartDemoHomeState extends State<RxDartDemoHome> {
+  PublishSubject<String> _textfieldSubject;
   @override
   void initState() {
     super.initState();
+    _textfieldSubject = PublishSubject<String>();
+    _textfieldSubject
+    .map((item) => 'map $item')
+    .where((item) => item.length > 9)
+    .debounce(Duration(milliseconds:500))
+    .listen((data) => print(data));
+    // _textfieldSubject.close();
     // Observable<String> _observable =
     //  Observable(Stream.fromIterable(['hello', '您好']));
     //Observable继承于Stream
@@ -36,20 +44,27 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
 
     //如果是BehaviorSubject, 只取最后一次add, 进行listen
     // PublishSubject<String> _subject = PublishSubject<String>();
-    ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 2);
-    _subject.add('add hello');
-    _subject.add('add world');
-    _subject.add('3rd add');
-    _subject.listen((data) => print('listen on 1: $data'));
-
-    _subject.listen((data) => print('listen on 2: ${data.toUpperCase()}'));
+    // ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 2);
+    // _subject.add('add hello');
+    // _subject.add('add world');
+    // _subject.add('3rd add');
+    // _subject.listen((data) => print('listen on 1: $data'));
+    // _subject.listen((data) => print('listen on 2: ${data.toUpperCase()}'));
 
     //subject 相当于Stream中的Controller, 控制着Observable
-    _subject.close();
+    // _subject.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Theme(
+      data: Theme.of(context).copyWith(primaryColor: Colors.black),
+      child: TextField(
+        decoration: InputDecoration(labelText: 'Title', filled: true),
+        onChanged: (value) {
+          _textfieldSubject.add('input: $value');
+        },
+      ),
+    );
   }
 }
